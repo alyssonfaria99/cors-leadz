7c07acf1975080da8f4c82abae99a50b2ba80f30
Alysson Faria <alyssonfaria99@gmail.com>
2024-10-30 09:18:47 -0300

    funcionando no insomnia

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 // Middleware para definir o cabeçalho Content-Security-Policy
-app.use((req, res, next) => {
-    // Permite scripts apenas do próprio servidor
-    res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self'");
-    next();
-});
+// app.use((req, res, next) => {
+//     // Permite scripts apenas do próprio servidor
+//     res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self'");
+//     next();
+// });
 
 // Middleware para permitir JSON no corpo das requisições
 app.use(express.json());
@@ -20,17 +20,21 @@ app.use(express.json());
 // Rota para evitar erro 503 no favicon
 app.get('/favicon.ico', (req, res) => res.status(204));
 
+app.get('/', (req, res) => {
+    console.log('ok');
+    res.status(200).send('ok');
+})
+
 // Rota para o proxy
 app.post('/proxy', async (req, res) => {
     try {
         // Substitua 'YOUR_SCRIPT_URL' pela URL do Google Apps Script copiada anteriormente
-        const googleAppsScriptUrl = 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec';
+        const googleAppsScriptUrl = 'https://script.google.com/macros/s/AKfycbxaxcz6uaZGue8kWm-mb4ruND02Fk6xGvvqCYeEqfAxYJA5YOOLjGMBPMB9v1BCyP03/exec';
 
         // Encaminha a requisição para o Google Apps Script com os dados recebidos
         const response = await axios.post(googleAppsScriptUrl, req.body, {
             headers: { 'Content-Type': 'application/json' }
         });
-
         // Envia a resposta de volta para o cliente (WaSpeed)
         res.status(response.status).json(response.data);
     } catch (error) {
